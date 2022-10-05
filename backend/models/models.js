2bfca151e751e807c12b274b248bdf33bb88ab56
Alex <navia09alex64@gmail.com>
2022-10-04 21:22:42 -0500

    Test?

--- backend/models/models.js
+++ backend/models/models.js
@@ -112,7 +112,7 @@ let orgDataSchema = new Schema({
         type: String
     },
     orgEvents: [String],
-    
+
 }, {
     collection: 'orgData',
     timestamps: true
@@ -126,4 +126,4 @@ const eventdata = mongoose.model('eventData', eventDataSchema);
 const orgdata = mongoose.model('orgData', orgDataSchema);
 
 // package the models in an object to export 
-module.exports = { primarydata, eventdata,orgdata }
+module.exports = { primarydata, eventdata, orgdata }
