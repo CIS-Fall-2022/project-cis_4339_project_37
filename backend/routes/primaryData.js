b4a5465657583f2726f8399f2090d4db6ab95387
Alex <navia09alex64@gmail.com>
2022-10-06 15:16:44 -0500

    polished up routes for primaryData; added comments to models schemas

--- backend/routes/primaryData.js
+++ backend/routes/primaryData.js
@@ -4,10 +4,18 @@ const router = express.Router();
 //importing data model schemas
 let { primarydata } = require("../models/models");
 let { eventdata } = require("../models/models");
+// IMPORTANT - "/" route will only return the clients that are apart of the organization
+// that is running the instance. So depending on id that is stored in the .env file ex: ORGANIZATION = 'XXXXXXXXX-452e-11ed-a044-27e6ab98a1a8'
+// some routes will only return specifically for that org
 
-//GET all entries
+
+// ----------------- GET REQUESTS ----------------------------
+//GET request that retrieves all clients for the org that 
+// running the instance, so it depends on the .env variable for organization
 router.get("/", (req, res, next) => {
-    primarydata.find(
+    // grabs the organization from the .env file
+    orgID = process.env.ORGANIZATION
+    primarydata.find({ orgs: orgID },
         (error, data) => {
             if (error) {
                 return next(error);
@@ -18,7 +26,7 @@ router.get("/", (req, res, next) => {
     ).sort({ 'updatedAt': -1 }).limit(10);
 });
 
-//GET single entry by ID
+//GET request that retrives single entry by using the ID of the client
 router.get("/id/:id", (req, res, next) => {
     primarydata.find(
         { _id: req.params.id },
@@ -32,13 +40,7 @@ router.get("/id/:id", (req, res, next) => {
     );
 });
 
-
-
-
-
-
-
-//GET entries based on search query
+//GET requests that retrieves clients based on search query
 //Ex: '...?firstName=Bob&lastName=&searchBy=name' 
 router.get("/search/", (req, res, next) => {
     let dbQuery = "";
@@ -61,12 +63,8 @@ router.get("/search/", (req, res, next) => {
     );
 });
 
-//GET events for a single client
-router.get("/events/:id", (req, res, next) => {
-
-});
-
-//POST
+// ----------- POST request --------------------
+//POST request to insert a client into the db
 router.post("/", (req, res, next) => {
     primarydata.create(
         req.body,
@@ -83,7 +81,9 @@ router.post("/", (req, res, next) => {
     primarydata.createdAt instanceof Date;
 });
 
-//PUT update (make sure req body doesn't have the id)
+// ------------------ PUT Requests --------------------------------
+
+//PUT request to update the attributes for the client using the id of the client
 router.put("/:id", (req, res, next) => {
     primarydata.findOneAndUpdate(
         { _id: req.params.id },
@@ -98,12 +98,16 @@ router.put("/:id", (req, res, next) => {
     );
 });
 
+
+// PUT request to add an org to client
 router.put("/addOrg/:id", (req, res, next) => {
-    console.log(req.body)
+    // use org ID from .env file of that org's instance
+    orgID = process.env.ORGANIZATION
     primarydata.updateOne(
         { _id: req.params.id },
+        // $push to insert the id into the org array
         {
-            $push: req.body
+            $push: { orgs: orgID }
         },
         (error, data) => {
             if (error) {
@@ -115,6 +119,29 @@ router.put("/addOrg/:id", (req, res, next) => {
     );
 });
 
+
+// removes org to a client in order to associate them
+router.put("/removeOrg/:id", (req, res, next) => {
+    //contains the ID of the organization that is running this instance
+    orgID = process.env.ORGANIZATION
+    primarydata.updateOne(
+        // finds the user
+        { _id: req.params.id },
+        // pulls org from the array that contains org ids 
+        {
+            $pull: { orgs: orgID }
+        },
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+// ------------- DELETE Requests ----------------------------
 // Deletes a client based on their id
 router.delete("/deletePrimary/:id", (req, res, next) => {
     console.log(req.body)
